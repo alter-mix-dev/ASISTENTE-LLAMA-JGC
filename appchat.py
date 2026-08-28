af7f930ee9f3734c208c4217f0a320cd9b9f7d0d
alter-mix-dev <alter.mix.dev@gmail.com>
2026-08-28 11:11:48 -0600

    Implement clear chat history button
    Added a button to clear chat history.

--- appchat.py
+++ appchat.py
@@ -141,7 +141,8 @@ if user_query:
     st.session_state.chat_history.append({"role": "assistant", "content": response_text})
 ################### CODIGO NUEVO AGREGADO###########################
 #boton para limpiar el historial
-"""if st.button("🗑️ Limpiar Historial"):
+"""
+if st.button("🗑️ Limpiar Historial"):
     st.session_state.messages = []
     st.rerun()
     #boton para subir un nuevo pdf
@@ -162,7 +163,8 @@ st.download_button(
     data=historial_texto,
     file_name="historial_chat.txt",
     mime="text/plain"
-)"""
+)
+"""
 #estructura de la barra lateral izquierda que contiene los botones
 # --- SECCIÓN DE ACCIONES EN LA BARRA LATERAL ---
 with st.sidebar:
